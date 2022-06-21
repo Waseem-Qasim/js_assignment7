92db93dc870005fc0e29316c2b6543e961f1d546
waseem-qasim <waseemqasim@gmail.com>
2022-06-21 19:52:23 +0500

    first commit

---
+++ aap.js
@@ -0,0 +1,99 @@
+//Assignment 7 Q=1//
+//===========================================//
+
+// var input1 = prompt("Enter Your First Name")
+// var input2 = prompt("Enter Your Last Name")
+// document.write(input1 + " " + input2);
+
+
+
+
+// //Assignment 7 Q=2//
+// //===========================================//
+
+// var userInput = prompt("Enter Your Favorite Mobile Name"); 
+
+// document.write(
+//   "My Favorite Mobile is: " + userInput + "<br>" + "Lenght of String: " + userInput.length
+// );
+
+
+
+
+
+// //Assignment 7 Q=3//
+// //===========================================//
+
+// var word = "Pakistani"
+// document.write("String of: " + word + "<br>" + "Index of N is: " + word.indexOf("n"))
+
+
+
+
+
+//Assignment 7 Q=4//
+//===========================================//
+
+// var word = "Hello World"
+// document.write("String of: " + word + "<br>" + "Index of L is: " + word.lastIndexOf("l"))
+
+
+
+
+
+
+//Assignment 7 Q=5//
+//===========================================//
+
+// var word = "Pakistani"
+// document.write("String of: " + word + "<br>" + "Character at Index 3 is: " + word.charAt(3))
+
+
+
+
+
+//Assignment 7 Q=6//
+//===========================================//
+
+// var input1 = prompt("Enter Your First Name")
+// var input2 = prompt("Enter Your Last Name")
+// var firstWord = (input1)
+// var lastWord = (input2)
+// document.write (firstWord.concat(lastWord));
+
+
+
+
+
+
+// //Assignment 7 Q=7//
+// //===========================================//
+
+// var city = ("Hyderabad")
+
+// document.write ("City: " + city + "<br>" + "After Replacement: " + city.replace("Hyder", "Islam"))
+
+
+
+
+
+// //Assignment 7 Q=8//
+// //===========================================//
+
+// var text = ("<h1> Ali and Sami are best friends. They play cricket and football together. </h1>  <br> ")
+// document.write (text + "<br>" + " <h1> After Replacement: </h1> " + text.replaceAll("and", "&"))
+
+
+
+
+
+// //Assignment 7 Q=9//
+// //===========================================//
+
+var stringNumber = "472";
+
+document.write("Value : " + stringNumber + "<br>" + "Type :  " + typeof (stringNumber) + "<br>");
+
+var numberToString = +stringNumber;
+
+document.write("Value : " + numberToString + "<br>" + "Type :  " + typeof (numberToString));
